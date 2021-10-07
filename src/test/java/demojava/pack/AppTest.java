e00cd4e3ebe6a95cbacd5e1f41e39df4f57701fd
NguyenLeAuto <91585700+NguyenLeAuto@users.noreply.github.com>
2021-10-07 20:55:33 +0700

    Update AppTest.java

--- src/test/java/demojava/pack/AppTest.java
+++ src/test/java/demojava/pack/AppTest.java
@@ -30,9 +30,9 @@ public class AppTest
     {     System.out.println("code selenium testing");
     WebDriverManager.chromedriver().setup();
     ChromeOptions options = new ChromeOptions();
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--headless");
+   // options.addArguments("--no-sandbox");
+   // options.addArguments("--disable-dev-shm-usage");
+   // options.addArguments("--headless");
     driver = new ChromeDriver(options);
     driver.navigate().to("https://testdev.vetspire.com");
     driver.manage().window().maximize();
